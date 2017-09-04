28eb440b115e97c2d9c080b9853b1f4afce91dbb
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-09-05 04:38:07 +0530

    Dominant colour

---
+++ ModeCol.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy import stats
+import cv2
+import os
+
+files 	= os.listdir("Data/")
+print(files)
+pace 	= []
+writer 	= []
+for file in files:
+	cap = cv2.VideoCapture("Data/" + file)
+
+	print("Processing file " + file)
+	iterator = 0
+	while(True):
+		iterator = iterator + 1
+		
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+		if(iterator%100 == 0):
+	   		if(frame is None):
+	   			break
+			average_color = [stats.mode(frame[:, :, i]) for i in range(frame.shape[-1])]
+			average_color = [int(x) for x in average_color]
+			writer.append(average_color)
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()
+
+for i in range(100):
+	pace.append(writer)
+
+pace = np.asarray(pace)
+print(pace.shape)
+cv2.imwrite("ResultMode.jpg", pace)
