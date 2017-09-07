1cc9a63490f0177889d1dd62f7a617ba54927ce8
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-09-07 06:42:43 +0530

    File to combine and make a visual

---
+++ Combine.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+fig = plt.figure()
+ax1 = fig.add_subplot(2,2,1)
+ax1.imshow('Result.jpg')
+ax2 = fig.add_subplot(2,2,2)
+ax2.imshow('Result.jpg')
+plt.show()
