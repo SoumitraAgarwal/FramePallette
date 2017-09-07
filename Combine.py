560a8dce4289b528429f49f7991aa41e694e27c9
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-09-07 07:13:06 +0530

    Final combine and submit

--- Combine.py
+++ Combine.py
@@ -3,8 +3,16 @@ import matplotlib.image as mpimg
 import numpy as np
 
 fig = plt.figure()
-ax1 = fig.add_subplot(2,2,1)
-ax1.imshow('Result.jpg')
-ax2 = fig.add_subplot(2,2,2)
-ax2.imshow('Result.jpg')
+ax1 = fig.add_subplot(1,1,1)
+ax1.set_title("Average colour v/s Dominant colour")
+img = mpimg.imread('Result.png')
+ax1.imshow(img, aspect='auto')
+# ax1.axis('off')
+
+
+ax2 = fig.add_subplot(2,1,2)
+img = mpimg.imread('ResultMode.png')
+ax2.imshow(img, aspect='auto')
+ax2.axis('off')
+plt.tight_layout()
 plt.show()
